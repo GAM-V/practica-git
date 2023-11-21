bcebdf90c3fdc7ccaaab3eec2249eb17760cbe5a
Gustavo Angel Morales Vigi <125324187+GAM-V@users.noreply.github.com>
2023-11-21 09:11:52 -0600

    Add files via upload

---
+++ holaMundo.py
@@ -0,0 +1,8 @@
+def saludar(nombre, matricula):
+    print(f"Hola Mundo, {nombre}!")
+    print(f"Tu número de matrícula es: {matricula}")
+
+if __name__ == "__main__":
+    nombre = 'Gustavo'
+    matricula = '2223029054'
+    saludar(nombre, matricula)
